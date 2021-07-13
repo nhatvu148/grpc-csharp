e985d9c8f819db75f96a631cb945d69a2b6d6d6a
nhatvu148 <nhatvu148@gmail.com>
2021-07-13 20:37:59 +0900

    add client

--- client/Program.cs
+++ client/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Dummy;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
 
 namespace client
 {
     class Program
     {
+        const string target = "127.0.0.1:50051";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith(task =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully");
+                }
+            });
+
+            var client = new DummyService.DummyServiceClient(channel);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
         }
     }
 }
