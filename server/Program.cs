f9fd84b2c3e7086f6b6ba1791b090ec273a864f1
nhatvu148 <nhatvu148@gmail.com>
2021-07-13 20:23:02 +0900

    add dummy proto

--- server/Program.cs
+++ server/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
+using System.IO;
 
 namespace server
 {
     class Program
     {
+        const int Port = 50051;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Server server = null;
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine("The server is listening on port: " + Port);
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The server failed to start:" + e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
         }
     }
 }
